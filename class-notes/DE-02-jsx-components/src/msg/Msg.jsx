fb9e52945fee1f31613f4a0e73578b769fd4c782
Nihal Tekin <nihalpolattekin@gmail.com>
2023-05-09 12:31:16 +0200

    second commit

---
+++ class-notes/DE-02-jsx-components/src/msg/Msg.jsx
@@ -0,0 +1,15 @@
+// rafce: reactArrowfonktionExportComponent
+ // rfce: burada da const yerine fonktion yazar 
+
+
+import React from 'react'
+
+const Msg = () => {
+
+//! JAVASCRIPT ALANI ifler forlar buraya
+  return (
+    <div>Msg</div>
+  )
+}
+
+export default Msg
