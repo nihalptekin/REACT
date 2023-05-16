9b318211181d171b7dd1398f8179c82e23adc8fa
Nihal Tekin <nihalpolattekin@gmail.com>
2023-05-16 12:35:38 +0200

    From jsx

--- class-notes/DE-05-06-events-hooks/src/App.js
+++ class-notes/DE-05-06-events-hooks/src/App.js
@@ -2,17 +2,26 @@ import React from "react";
 import Events from "./1-Events-hooksintro/Events";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Hooks from "./2-hooks/Hooks";
-import Counter from "./3-classComponents/Counter";
+import Form from "./3-forms/Form";
+import Counter from "./2-classComponents/Counter";
+import MouseKeyboard from "./4-mouse-keyBoard/MouseKeyboard";
+
+
 
 
 
 function App() {
   return (
-    <div >
-     <Events/>
+    <div>
+     {/* <Events/>
      <Hooks/>
      <Counter/>
-     <Counter count={20}/>
+     <Counter count={20}/> */}
+    
+ {/* <Form/> */}
+
+ <MouseKeyboard/>
+
     </div>
   );
 }
