3687cdc404691ae151b21a9519e01791d01ec819
nihal tekin <nihalpolattekin@gmail.com>
2023-06-12 17:17:08 +0200

    aa

--- class-notes/DE-21-Material-UI/src/components/CardsGrid.jsx
+++ class-notes/DE-21-Material-UI/src/components/CardsGrid.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Container,
+  Grid,
+  Typography,
+} from "@mui/material";
+import DeleteSweepIcon from '@mui/icons-material/DeleteSweep';
+import SendIcon from '@mui/icons-material/Send';
+import React, { useEffect, useState } from "react";
 
 const CardsGrid = () => {
-  return (
-    <div>CardsGrid</div>
-  )
-}
+  const [products, setProducts] = useState([]);
 
-export default CardsGrid
+  useEffect(() => {
+    fetch("https://dummyjson.com/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data.products));
+  }, []);
+
+  return (
+    <Container>
+      <Typography variant="h4" align="center" color="error">
+        CARD & GRID
+      </Typography>
+
+      <Grid container mt={4} spacing={2}>
+        {products.map((a) => {
+          return (
+            <Grid item xs={6} md={4}>
+              <Card sx={{ maxWidth: 345, height:450, }}>
+                <CardMedia
+                  sx={{ height: 200 }}
+                  image={a.images[0]}
+                //  alt="resim"
+                />
+                <CardContent  >
+                  <Typography gutterBottom variant="h5" component="div">
+                    {a.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {a.description}
+                  </Typography>
+                </CardContent>
+             <CardActions>
+                  <Button variant="contained" size="small" startIcon={<SendIcon/>}>Share</Button>
+                  <Button variant="contained" size="small" endIcon={<DeleteSweepIcon/>}>DELETE</Button>
+                </CardActions>
+               
+              </Card>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Container>
+  );
+};
+
+export default CardsGrid;
--- class-notes/DE-21-Material-UI/src/components/TextFieldComp.jsx
+++ class-notes/DE-21-Material-UI/src/components/TextFieldComp.jsx
@@ -1,11 +1,59 @@
-import { Container, Typography } from '@mui/material'
+import { Box, Button, Container, FormControl, Input, InputAdornment, InputLabel, TextField, Typography } from '@mui/material'
+import AccountCircle from "@mui/icons-material/AccountCircle";
 import React from 'react'
 
 const TextFieldComp = () => {
+  let error1=true
+
+
   return (
     <div>
       <Container>
 <Typography variant='h4' align='center' color="error" >TEXT-FIELD (INPUT)</Typography>
+<Box sx={{mt:4}}>
+<TextField margin="normal"
+          required
+          id="outlined-required"
+          label="Required"
+          defaultValue="Hello World"
+          type='email'
+          fullWidth
+          // fullWidth ile kutucuk sayfa boyunca kapladi
+        />
+        <TextField
+          required
+          id="outlined-required"
+          label="Required"
+          defaultValue="Hello World"
+          variant='filled'
+        />
+        {/* defaultta label input outline cizgisinde gelir, varianti=filled diyerek label'i input alanina alabiliriz */}
+        <TextField
+        margin='normal'
+        fullWidth
+          error={error1}
+          id="password"
+          type='password'
+          placeholder='password giriniz'
+          // label="Error"
+          // defaultValue="Hello World"
+          helperText={error1 && "yanlis girdiniz"}
+        />
+         <FormControl fullWidth margin="normal" variant="standard">
+        <InputLabel htmlFor="input-with-icon-adornment">
+          With a start adornment
+        </InputLabel>
+        <Input
+          id="input-with-icon-adornment"
+          startAdornment={
+            <InputAdornment position="start">
+              <AccountCircle color='error'/>
+            </InputAdornment>
+          }
+        />
+      </FormControl>
+        <Button fullWidth variant='contained' color='error'>SUBMIT</Button>
+</Box>
       </Container>
     </div>
   )
