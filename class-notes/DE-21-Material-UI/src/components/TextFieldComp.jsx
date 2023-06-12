c5ca7f92ee4189d45d5c0de36936dbe7c76f14e3
nihal tekin <nihalpolattekin@gmail.com>
2023-06-12 10:51:44 +0200

    material-ui

---
+++ class-notes/DE-21-Material-UI/src/components/TextFieldComp.jsx
@@ -0,0 +1,14 @@
+import { Container, Typography } from '@mui/material'
+import React from 'react'
+
+const TextFieldComp = () => {
+  return (
+    <div>
+      <Container>
+<Typography variant='h4' align='center' color="error" >TEXT-FIELD (INPUT)</Typography>
+      </Container>
+    </div>
+  )
+}
+
+export default TextFieldComp
