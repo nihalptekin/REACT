196091fe5e31e5502890beb1a5c2e0b7d53ba556
nihal tekin <nihalpolattekin@gmail.com>
2023-06-22 10:43:58 +0200

    products(stockApp)

--- class-notes/DE-24_29-StockApp/inclass/src/pages/Products.jsx
+++ class-notes/DE-24_29-StockApp/inclass/src/pages/Products.jsx
@@ -1,8 +1,125 @@
+import { useEffect, useState } from "react";
+import useStockCall from "../hooks/useStockCall";
+import Typography from "@mui/material/Typography";
+import { Button } from "@mui/material";
+import { useSelector } from "react-redux";
+import ProductModal from "../components/ProductModal";
+import Box from '@mui/material/Box';
+import { DataGrid } from '@mui/x-data-grid';
+
 
 const Products = () => {
-  return (
-    <div>Products</div>
-  )
-}
+  const { getStockData } = useStockCall();
+  const { products } = useSelector(state => state.stock);
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setOpen(false);
+    setInfo({
+      name: "",
+      cataegory_id: "",
+      brand_id: "",
+    });
+    //* handleClose olduğunda yani modal kapnadığında formdaki verilerin temizlenmesi için burada tanımladık.
+  };
+  const [info, setInfo] = useState({
+    name: "",
+    cataegory_id: "",
+    brand_id: "",
+  });
 
-export default Products
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    {
+      field: 'firstName',
+      headerName: 'First name',
+      width: 150,
+      editable: true,
+    },
+    {
+      field: 'lastName',
+      headerName: 'Last name',
+      width: 150,
+      editable: true,
+    },
+    {
+      field: 'age',
+      headerName: 'Age',
+      type: 'number',
+      width: 110,
+      editable: true,
+    },
+    {
+      field: 'fullName',
+      headerName: 'Full name',
+      description: 'This column has a value getter and is not sortable.',
+      sortable: false,
+      width: 160,
+      valueGetter: (params) =>
+        `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+    },
+  ];
+  
+  const rows = [
+    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  ];
+  
+
+
+  useEffect(() => {
+    getStockData("products");
+    getStockData("categories");
+    getStockData("brands")
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div>
+      <Typography variant="h4" color="error" mb={3}>
+        Products
+      </Typography>
+      <Button variant="contained" onClick={handleOpen}>
+        New Product
+      </Button>
+      <ProductModal
+        open={open}
+        handleClose={handleClose}
+        info={info}
+        setInfo={setInfo}
+      />
+       <Box sx={{ height: 400, width: '100%' }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 5,
+              },
+            },
+          }}
+          pageSizeOptions={[5]}
+          checkboxSelection
+          disableRowSelectionOnClick
+        />
+      </Box>
+      
+    </div>
+  );
+};
+
+export default Products;
+
+
+
+
+
+
